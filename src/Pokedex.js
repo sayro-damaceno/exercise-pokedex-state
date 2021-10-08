4fc9ee2320bb12afe779e2c7f176c91ae8d3036b
Sayro <sayrodamaceno70@gmail.com>
2021-10-08 10:20:28 -0300

    exercicio 1 do dia 1 do bloco 11

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,11 +2,44 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+      super()
+
+      this.state ={
+        pokeIndex: 0
+      }
+
+      this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    handleClick() {
+      this.setState((state, props) => {
+        if (state.pokeIndex === props.pokemons.length -1) {
+          return {
+            pokeIndex: 0
+          }
+        }
+        return {
+        pokeIndex: state.pokeIndex + 1
+      }})
+    }
+
+
     render() {
         return (
+          <>
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <div>{this.state.pokeIndex}</div>
+                {this.props.pokemons.filter((_pokemon, index) => index === this.state.pokeIndex).map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
             </div>
+            <button className='next-pokemon' onClick={this.handleClick}>Next Pokémon</button>
+
+            <div className='btns-types'>
+              <button className='type' onClick={this.handleClick}>Fire</button>
+              <button className='type' onClick={this.handleClick}>Psychic</button>
+            </div>
+          </>
         );
     }
 }
